af966addcdcdeda2d490cf21d69b97cc0dddfb99
hr-wang <hairongwng@gmail.com>
2014-05-14 11:58:19 +0200

    First test commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a test repository created on 5/14/2014.
---
+++ hello.c
@@ -0,0 +1,6 @@
+//Print Hello World! to the screen. 
+#include <stdio.h> 
+int main(int argc, char** argv){
+	printf("Hello World!\n");
+	return 0;
+}
